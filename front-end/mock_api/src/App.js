9f90cd217df230155ed4da80532a7d416f2c5bed
tzanlam <tzanlam@gmail.com>
2024-07-27 11:28:26 +0700

    code

--- front-end/mock_api/src/App.js
+++ front-end/mock_api/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
+import ModalHome from './components/ModalHome';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ModalHome/>
   );
 }
 
---
+++ front-end/mock_api/src/components/ModalHome.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Modal, Card, Button } from "antd";
+import "../assets/ModalHome.css";
+
+const ModalHome = () => {
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <Modal
+      title="Đăng nhập để nhận phiếu giảm giá"
+      open={isModalVisible}
+      footer={null}
+      onCancel={handleOk}
+    >
+      <Card title="GIẢM 50,000₫">
+        <p>Cho đơn hàng từ 200,000₫</p>
+        <p>Cho tất cả sản phẩm</p>
+      </Card>
+      <Card title="GIẢM 5%">
+        <p>Cho đơn hàng từ 600,000₫</p>
+        <p>Cho tất cả sản phẩm</p>
+      </Card>
+      <Card title="GIẢM 10%">
+        <p>Cho đơn hàng từ 1,000,000₫</p>
+        <p>Cho tất cả sản phẩm</p>
+      </Card>
+      <Button type="primary" block>
+        Thu thập tất cả
+      </Button>
+    </Modal>
+  );
+};
+
+export default ModalHome;
